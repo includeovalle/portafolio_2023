0669dff8f97c23fb0c3183850dab61b00c95f973
includeovalle <ovalle.carlosr@gmail.com>
2024-01-13 20:15:12 -0600

    update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,6 @@
 import {Card, Title, Languages, SocialMedia, EmailButton } from '@/app/components/Designs'
 import {RowContainer} from '@/app/components/'
 import Logo from '@/public/logo.png'
-import type { Metadata } from 'next'
-
-export const metadata : Metadata = {
-  openGraph: {
-    title: 'Bienvenido a mi página web'
-    , description: 'Soy Carlos Amaro, desarrollador web, me especializo en el desarrollo de aplicaciones web y sitios web. Me gusta trabajar con tecnologías como React, Nextjs, Nodejs, Express, MongoDB, MySQL, TypeScript, JavaScript, HTML, CSS, SASS, entre otras.'
-    , type: 'website'
-  },
-}
 
 export default function Home() {
 

2b2e761589df33c58c5f3af451d142b5b4b141df
includeovalle <ovalle.carlosr@gmail.com>
2024-04-08 18:04:25 -0600

    fix: [side-nav] fix popup, add iframodal

--- app/components/Designs/media/index.tsx
+++ app/components/Designs/media/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-import {SocialMedia, CustomImage} from "@/app/components/";
+import { SocialMedia, CustomImage, IfraModal } from "@/app/components/";
 import Whatsapp from "@/public/whatsapp.svg";
 import Github from "@/public/github.svg";
 import Linkedin from "@/public/linked.svg";
@@ -10,23 +10,33 @@ import Pdf from "@/public/pdf-icon.svg";
 
 
 const Index = () => {
-  const whatsappAPI = window.matchMedia("(max-width: 768px)").matches? "https://api.whatsapp.com/send?phone=4441934444" :  "https://web.whatsapp.com/send?phone=4441934444"
+  const whatsappAPI = window.matchMedia("(max-width: 768px)").matches ? "https://api.whatsapp.com/send?phone=4441934444" : "https://web.whatsapp.com/send?phone=4441934444"
 
- const text = "Links";
+  const text = "Links";
 
   const MediaInfo = [
-    {image: Whatsapp, alt: "contacto",
-      links:{href:whatsappAPI, target: "_blank"} }
-    ,{image: Github, alt: "repositori del proyecto",
-      links:{href: "https://github.com/includeovalle/portafolio_2023/", target: "_blank"} }
-    ,{image: Linkedin, alt: "contacto linkedin",
-      links:{href: "https://www.linkedin.com/in/carlos-amaro-dev", target: "_blank"}}
-    ,{image: Email, alt: "correo profesional",
-      links:{href: "mailto:carlosovalle@soultech.studio", target: "_blank"}}
-    ,{image: Story, alt: "pagina de testeo de componentes",
-      links:{href: "https://portafolio-2023-dayu.vercel.app/?path=/docs/configure-your-project--docs", target: "_blank"}}
-    ,{image: Pdf, alt: "Resumen profesional",
-      links:{href: "/carlos-amaro-resume.pdf", target: "_self"}}
+    { image: Whatsapp , alt: "contacto", links: { href: whatsappAPI , target: "_blank" }
+    }
+    , {
+      image: Github, alt: "repositori del proyecto",
+      links: { href: "https://github.com/includeovalle/portafolio_2023/", target: "_blank" }
+    }
+    , {
+      image: Linkedin, alt: "contacto linkedin",
+      links: { href: "https://www.linkedin.com/in/carlos-amaro-dev", target: "_blank" }
+    }
+    , {
+      image: Email, alt: "correo profesional",
+      links: { href: "mailto:carlosovalle@soultech.studio", target: "_blank" }
+    }
+    , {
+      image: Story, alt: "pagina de testeo de componentes",
+      links: { href: "https://portafolio-2023-dayu.vercel.app/?path=/docs/configure-your-project--docs", target: "_blank" }
+    }
+    , {
+      image: Pdf, iframe: true, alt: "Resumen profesional",
+      links: { href: "/carlos-amaro-resume.pdf", target: "_self" }
+    }
   ]
 
   return (
@@ -34,9 +44,17 @@ const Index = () => {
 
       <SocialMedia buttonText={text}>
         {
-          MediaInfo.map((item, index)=> (
-            <CustomImage key={index} src={item.image} className={"icon"} alt={item.alt} links={true} href={item.links.href} target={item.links.target}/>
-          ))
+          MediaInfo.map((item, index) => {
+            if (item.iframe) {
+              return (<><IfraModal src={item.links.href} onClick > <CustomImage className="icon" key={index} src={item.image} alt={item.alt}/> </IfraModal></>)
+      
+
+            } else {
+              return (
+                <CustomImage key={index} src={item.image} className={"icon"} alt={item.alt} links={true} href={item.links.href} target={item.links.target} />
+              )
+            }
+          })
         }
       </SocialMedia>
     </>

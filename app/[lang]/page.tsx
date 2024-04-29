1041bba96038b737c5d8158d9a6807df6c1b767b
includeovalle <ovalle.carlosr@gmail.com>
2024-04-29 08:43:17 -0600

    build: [locales] add dependencies, middleware, i18n configuration

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,27 +1,21 @@
 import { Header, Card, ProjectsTag, LanguagesTag, Languages, SocialMedia, EmailButton, Footer } from '@/app/components/Designs';;
-import { redirect } from 'next/navigation';
 import { MainContainer, InnerContainer } from '@/app/components';
 import { Metadata } from 'next'
-import { getDictionary } from "./dictionaries.js"
-
+import { getDictionary} from "./dictionaries"
+import { type Locale } from "../../i18n-config";
 export const metadata: Metadata = {
   title: 'Bienvenido a mi página web'
   , description: 'Soy Carlos Amaro, desarrollador web, me especializo en el desarrollo de aplicaciones web y sitios web. Me gusta trabajar con tecnologías como React, Nextjs, Nodejs, Express, MongoDB, MySQL, TypeScript, JavaScript, HTML, CSS, SASS, entre otras.'
 }
 
 interface Props {
-  params: { lang: string }
+  params: { lang: Locale }
   searchParams: URLSearchParams
 }
 
 export default async function Home({ searchParams, params }: Props) {
 
-  const validLangs = ["es", "en", "pt"];
-  const language = params.lang || "es";
-
-  if (!validLangs.includes(language)) {
-    return redirect("/es")
-  }
+  const language = params.lang;
 
   const lang = await getDictionary(language)
 
--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { 
-  i18n: {
-    locales: ['en', 'es', 'pt'],
-    defaultLocale: 'es',
-  },
-}
-module.exports = nextConfig

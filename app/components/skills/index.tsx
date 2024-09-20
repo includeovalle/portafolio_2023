cb65aba4baafefdb50b64e7eb3a4eb7cbe797457
includeovalle <ovalle.carlosr@soultech.studio>
2024-09-20 07:08:17 -0600

    feature(skills): start skills component

---
+++ app/components/skills/index.tsx
@@ -0,0 +1,38 @@
+
+import React from "react";
+import { CustomImage} from "../";
+import picture from "@/public/carlos.webp";
+import { classNamesInterface } from "@/app/types/";
+
+
+
+  interface IndexProps extends classNamesInterface {
+  text: string;
+}
+
+const Index = ({text, className}:IndexProps) => {
+
+  const length = text.length;
+  const deg = 360 / length;
+
+
+  return (
+    <CustomImage className={className} width={220} height={220}  figcaption={true} src={picture} alt="carlos raul amaro ovalle">
+        <div className="spinning-text">
+            { text && text.split("").map((letra, i) => (
+              <span
+                key={i}
+                style={{
+                  transform: `rotate(${deg * i}deg)`,
+                }}
+              >
+                {letra}
+              </span>
+            ))}
+        </div>
+    </CustomImage>
+  );
+};
+
+export default Index;
+

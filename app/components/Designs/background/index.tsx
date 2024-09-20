3e30ecea32205ee5b1df12d0935311d44b3553f6
includeovalle <ovalle.carlosr@soultech.studio>
2024-09-20 07:06:22 -0600

    chore(animations): update animations

--- app/components/Designs/background/index.tsx
+++ app/components/Designs/background/index.tsx
@@ -5,7 +5,7 @@ import {Ul} from "@/app/components"
 const Index = ({theme="default",children}:{theme:string,children?: React.ReactNode;}) => {
 
  const customClass = `background__${theme}`;
-  const LIS = ["","","","","","","","","",""]
+  const LIS = ["software","engineer","","","software","","","engineer","",""]
   return (
     <Ul lis={LIS} className={customClass}>
       {children}
